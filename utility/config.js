60ae9689ed49daf64e81d872174ce06a736eccd1
emil mustonen <emppu.mustonen1@gmail.com>
2023-03-25 19:29:57 +0200

    first draft of app

---
+++ utility/config.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+
+const PORT = process.env.PORT
+const user = process.env.PGUSER
+const host = process.env.PGHOST
+const database = process.env.PGDATABASE
+const password = process.env.PGPASSWORD
+const port = process.env.PGPORT
+
+module.exports = {
+  PORT, user, host, database, password, port
+}
